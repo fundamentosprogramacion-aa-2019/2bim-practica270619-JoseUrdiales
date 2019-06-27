c6aeb88900cb45702a11de61f2b925c2b4b2142f
JoseUrdiales <jose-urdiales@hotmail.com>
2019-06-27 18:16:52 -0500

    Tutoria 27/06/2019

---
+++ EjemploClases/src/ejemploclases/Portatil.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemploclases;
+
+/**
+ *
+ * @author Jose Luis
+ */
+public class Portatil {
+
+    String color;
+    String procesador;
+    public void establecerColor(String c){
+        color  = c;
+    }
+    public String obtenerColor(){
+        return color;
+    }
+    public void establecerProcesador(String p){
+        color  = p;
+    }
+    public String obtenerProcesador(){
+        return procesador;
+    }
+}
